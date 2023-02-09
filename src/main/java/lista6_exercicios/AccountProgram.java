c94f0ca8ae4857114feb49c06dbc837af3d807c8
Alexandre <alexandrerider@hotmail.com>
2023-02-09 10:48:30 -0300

    exercicio de orientação a objetos: algumas refatorações para o AccountProgram, para diminuir as requisições de sout.

--- src/main/java/lista6_exercicios/AccountProgram.java
+++ src/main/java/lista6_exercicios/AccountProgram.java
@@ -1,5 +1,6 @@
 package lista6_exercicios;
 
+import java.util.Formatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,53 +8,63 @@ public class AccountProgram {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
         Scanner scanner = new Scanner(System.in);
+
         int number;
         String name;
-        char initialDepositConfirm;
         double initialDeposit;
-
 
         Account account;
         System.out.print("Enter account number: ");
         number = scanner.nextInt();
+
         System.out.print("Enter account holder name: ");
         name = scanner.nextLine();
         name += scanner.nextLine();
-        System.out.print("is there initial deposit (y/n)? ");
-        initialDepositConfirm = scanner.next().charAt(0);
 
+        if (initialDepositRequest(scanner.next().charAt(0))){
 
-        if (initialDepositConfirm == 'y'){
             System.out.print("Enter deposit: ");
             initialDeposit = scanner.nextDouble();
             account = new Account(number, name, initialDeposit);
-            System.out.printf("\nAccount data%nAccount %d, Holder: %s, Balance: $ %.2f%n",
-                    account.getNumberAccount(), account.getNameAccount(),account.getDeposit());
+
+            printData(account.getNumberAccount(),account.getNameAccount(),account.getDeposit());
 
             System.out.print("Enter a deposit value: ");
             account.deposit(scanner.nextDouble());
-            System.out.printf("Updated account data%nAccount %d, Holder: %s, Balance: $ %.2f%n",
-                    account.getNumberAccount(), account.getNameAccount(),account.getDeposit());
+            printUpdateAccountData(account.getNumberAccount(),account.getNameAccount(),account.getDeposit());
 
             System.out.print("Enter a withDraw value: ");
             account.withDraw(scanner.nextDouble());
-            System.out.printf("Updated account data%nAccount %d, Holder: %s, Balance: $ %.2f%n",
-                    account.getNumberAccount(), account.getNameAccount(),account.getDeposit());
-        }
-        account = new Account(number,name);
-        System.out.printf("\nAccount data%nAccount %d, Holder: %s, Balance: $ %.2f%n",
-                account.getNumberAccount(), account.getNameAccount(),account.getDeposit());
 
-        System.out.print("\nEnter a deposit value: ");
+            printUpdateAccountData(account.getNumberAccount(),account.getNameAccount(),account.getDeposit());
+            System.exit(0);
+        }
+
+        account = new Account(number,name);
+        printData(account.getNumberAccount(),account.getNameAccount(),account.getDeposit());
+
+        System.out.print("Enter a deposit value: ");
         account.deposit(scanner.nextDouble());
 
-        System.out.printf("Updated account data%nAccount %d, Holder: %s, Balance: $ %.2f%n",
-                account.getNumberAccount(), account.getNameAccount(),account.getDeposit());
+        printUpdateAccountData(account.getNumberAccount(),account.getNameAccount(),account.getDeposit());
 
-        System.out.print("\nEnter a withDraw value: ");
+        System.out.print("Enter a withDraw value: ");
         account.withDraw(scanner.nextDouble());
 
-        System.out.printf("Updated account data%nAccount %d, Holder: %s, Balance: $ %.2f%n",
-                account.getNumberAccount(), account.getNameAccount(),account.getDeposit());
+        printUpdateAccountData(account.getNumberAccount(),account.getNameAccount(),account.getDeposit());
+    }
+
+    public static void printData(int number, String name,double initialDeposit){
+        Formatter formatter = new Formatter();
+        formatter.format("Account data%nAccount %d, Holder: %s, Balance: $ %.2f%n",number,name,initialDeposit);
+        System.out.println(formatter);
+    }
+    public static void printUpdateAccountData(int number, String name, double initialDeposit){
+        Formatter formatter = new Formatter();
+        formatter.format("Updated account data%nAccount %d, Holder: %s, Balance: $ %.2f%n", number, name, initialDeposit);
+        System.out.println(formatter);
+    }
+    public static boolean initialDepositRequest(char yesOrNo){
+        return yesOrNo == 'y';
     }
 }
